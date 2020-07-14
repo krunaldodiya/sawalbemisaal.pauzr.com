0ff82ff7392e6bc6b5de649d7f9fd0fab6995fe7
kunal dodiya <kunal.dodiya1@gmail.com>
2020-07-14 12:02:55 +0530

    added nova permission for aryan

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                "kunal.dodiya1@gmail.com"
+                "kunal.dodiya1@gmail.com",
+                "aryanadya@gmail.com"
             ]);
         });
     }
