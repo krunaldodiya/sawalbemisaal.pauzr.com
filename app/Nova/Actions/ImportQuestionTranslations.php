cdb5fa0eb45e19139300606cf43876eba5c0ef79
kunal dodiya <kunal.dodiya1@gmail.com>
2020-08-12 15:50:55 +0530

    done

--- app/Nova/Actions/ImportQuestionTranslations.php
+++ app/Nova/Actions/ImportQuestionTranslations.php
@@ -6,7 +6,9 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
-use Laravel\Nova\Actions\Action;
+
+use Anaseqal\NovaImport\Actions\Action;
+
 use Laravel\Nova\Fields\ActionFields;
 
 use Laravel\Nova\Fields\File;
