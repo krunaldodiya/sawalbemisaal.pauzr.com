6b292aab6e3b6e6fcc2d1ac1d90d82d489042843
kunal dodiya <kunal.dodiya1@gmail.com>
2020-09-04 17:16:06 +0530

    done

--- app/Http/Requests/EditProfile.php
+++ app/Http/Requests/EditProfile.php
@@ -16,8 +16,8 @@ class EditProfile extends FormRequest
     {
         return [
             'name' => 'required|min:3',
-            'username' => 'required|unique:users,'.auth()->id(),
-            'email' => 'required|email|unique:users,'.auth()->id(),
+            'username' => 'required|unique:users,username,'.auth()->id(),
+            'email' => 'required|email|unique:users,email,'.auth()->id(),
             'dob' => 'required',
             'gender' => 'required',
         ];
