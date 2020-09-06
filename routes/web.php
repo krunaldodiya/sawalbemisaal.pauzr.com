9fc05744e88394a6ddf8122b8c4a46406b41487f
kunal dodiya <kunal.dodiya1@gmail.com>
2020-09-06 22:10:07 +0530

    done

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Carbon\Carbon;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    $timings = "07/09/2020 09:30 AM";
+
+    $quiz_starting_time = Carbon::createFromFormat('d/m/Y h:m A', $timings);
+
+    $expiry = $quiz_starting_time->diffInMinutes(now()->format("d/m/Y h:m A"));
+
+    return [
+        'expiry' => $expiry,
+    ];
 });
 
 Route::get('/media/{media}', 'HomeController@getMediaFile');
