a90e40937dfebdb5129f0a55f444b87e02d1c4d9
kunal dodiya <kunal.dodiya1@gmail.com>
2020-09-10 09:00:36 +0530

    added nova tinker

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -86,10 +86,11 @@ class QuizController extends Controller
         $quizzes = Quiz::with('host', 'participants', 'quiz_infos', 'rankings')
             ->where('expired_at', '>=', now()->startOfDay())
             ->where(function($query) {
-                return $query->where('host_id', auth()->id())
+                return $query
+                    ->where('host_id', auth()->id())
                     ->orWhereHas('participants', function ($query) {
                         return $query->where('user_id', auth()->id());
-                    })
+                    });
             })
             ->orderBy('expired_at', 'asc')
             ->get();
