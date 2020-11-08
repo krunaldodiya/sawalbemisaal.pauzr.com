bd5dc7a881ba2834fd7805f6d4af5ba8fd976f36
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-11-08 11:26:22 +0530

    finished submit quiz

--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,6 @@ Route::get('/test', function (Request $request) {
     return 'test';
 });
 
-Route::get('/test/push', function (
-    Request $request,
-    PushNotificationRepositoryInterface $pushNotificationRepositoryInterface
-) {
-    $topic = Topic::where(['notifiable_type' => 'quiz', 'notifiable_id' => $request->quiz_id])->first();
-
-    $pushNotificationRepositoryInterface->notify("/topics/{$topic->name}", [
-        'title' => 'test',
-        'body' => 'test',
-        'image' => url('images/notify_winners.png'),
-    ]);
-});
-
-Route::get('/test/subscribe', function (
-    Request $request,
-    PushNotificationRepositoryInterface $pushNotificationRepositoryInterface
-) {
-    $topic = Topic::where(['notifiable_type' => 'quiz', 'notifiable_id' => $request->quiz_id])->first();
-
-    return $pushNotificationRepositoryInterface->subscribeToTopic($topic->name, $request->user_id);
-});
-
 Route::get('/refer', function (Request $request) {
     if ($request->has('utm_id')) {
         $request->session()->put('utm_id', $request->get('utm_id'));
