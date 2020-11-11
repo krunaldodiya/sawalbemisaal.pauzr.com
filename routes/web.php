503bddff98121561491caf48375c693a50e7a2a1
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-11-11 21:13:12 +0530

    finished submit quiz

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Repositories\PushNotificationRepositoryInterface;
 use Illuminate\Support\Facades\Route;
 
 use Illuminate\Http\Request;
@@ -8,8 +9,14 @@ Route::get('/', function (Request $request) {
     return view('welcome');
 });
 
-Route::get('/test', function (Request $request) {
-    return 'test';
+Route::get('/test', function (Request $request, PushNotificationRepositoryInterface $pushNotificationRepositoryInterface) {
+    $pushNotificationRepositoryInterface->notify("/topics/quiz_c3b21b58-27c1-4c43-803f-de67e88c4305", [
+        'key' => $request->key,
+        'title' => 'Quiz will start in few minutes',
+        'body' => "Everyone is preparing, are you?",
+        'image' => url('images/notify_soon.jpg'),
+        'quiz_id' => "c3b21b58-27c1-4c43-803f-de67e88c4305",
+    ]);
 });
 
 Route::get('/refer', function (Request $request) {
