dc2270e727bacca1c2a3ee85618de711f2e6ea8e
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-11-18 13:02:44 +0530

    finished submit quiz

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->post('/plans/purchase', "PlanController@purch
 Route::middleware('auth:sanctum')->get('/payments/withdraw/history', "PaymentController@getWithdrawHistory");
 Route::middleware('auth:sanctum')->post('/payments/withdraw', "PaymentController@withdrawAmount");
 
-Route::middleware('auth:sanctum')->post('/stories', "StoryController@getStories");
+Route::middleware('auth:sanctum')->get('/stories', "StoryController@getStories");
 
 Route::middleware('auth:sanctum')->post('/quiz/host', "QuizController@host");
 Route::middleware('auth:sanctum')->post('/quiz/generate', "QuizController@generate");
