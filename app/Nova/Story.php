a742787a67de9b65bc35a878659f64f901993b47
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-11-26 23:10:23 +0530

    stories started

--- app/Nova/Story.php
+++ app/Nova/Story.php
@@ -25,7 +25,7 @@ class Story extends Resource
      *
      * @var string
      */
-    public static $title = 'id';
+    public static $title = 'title';
 
     /**
      * The columns that should be searched.
