3245f9e4718692279a71064675c01cac3372879a
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-12-21 13:47:52 +0530

    added redeem resource

--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -14,6 +14,8 @@ class PaymentResource extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+        ];
     }
 }
