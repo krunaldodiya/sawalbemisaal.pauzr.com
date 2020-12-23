f2561604f0110b1ad048408d5a1825a6e2d651ec
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-12-23 16:58:21 +0530

    added notification list

--- app/Listeners/NotifyFollowers.php
+++ app/Listeners/NotifyFollowers.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\QuizGenerated;
+use App\User;
 use App\Quiz;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
@@ -28,8 +29,8 @@ class NotifyFollowers implements ShouldQueue
      */
     public function handle(QuizGenerated $event)
     {
-        $quiz = Quiz::with('host')->find($event->quiz->id);
+        $host = User::find($event->quiz->host->id);
 
-        $quiz->host->followers()->notify(new QuizHosted($quiz));
+        $host->followers()->notify(new QuizHosted($event->quiz));
     }
 }
