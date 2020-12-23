82c39a151a5aff057a2d4691d167bd4badec06dc
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-12-23 11:32:10 +0530

    added follow notification

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -23,17 +23,12 @@ class FollowController extends Controller
                 Notification::send($following, new UserFollowed($following->toArray(), $user->toArray()));
             }
 
-
-
-
-            // if (empty($follow['detached'])) {
-            //     $following->notify(new UserFollowed($following->toArray(), $user->toArray()));
-            // } else {
-            //     $following->notifications()
-            //         ->where('data->following_id', $following->id)
-            //         ->where('data->follower_id', $user->id)
-            //         ->delete();
-            // }
+            if (empty($follow['attached'])) {
+                $following->notifications()
+                    ->where('data->following_id', $following->id)
+                    ->where('data->follower_id', $user->id)
+                    ->delete();
+            }
 
             return response(['success' => true], 200);
         } catch (\Throwable $th) {
