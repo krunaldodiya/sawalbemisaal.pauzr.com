c0c23fca8e8c7a3d51019e692b9b5d16c5ff6e81
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-12-23 11:11:24 +0530

    added follow notification

--- app/Notifications/UserFollowed.php
+++ app/Notifications/UserFollowed.php
@@ -20,37 +20,11 @@ class UserFollowed extends Notification
         $this->follower = $follower;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['database'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function toArray($notifiable)
     {
         return [
