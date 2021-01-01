9b1c79a1029842effdc74a704039dcce6415ea1d
Krunal Dodiya <kunal.dodiya1@gmail.com>
2021-01-01 16:13:40 +0530

    added follow admin

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -2,10 +2,18 @@
 
 namespace App\Nova;
 
+use App\Nova\Actions\WalletPoint;
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\Image;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\BelongsToMany;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\HasOne;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 
 class User extends Resource
@@ -16,6 +24,8 @@ class User extends Resource
      * @var string
      */
     public static $model = \App\User::class;
+
+    public static $group = 'User';
 
     /**
      * The single value that should be used to represent the resource when being displayed.
@@ -33,6 +43,8 @@ class User extends Resource
         'id', 'name', 'email',
     ];
 
+    public static $orderBy = ['created_at' => 'desc'];
+
     /**
      * Get the fields displayed by the resource.
      *
@@ -44,11 +56,25 @@ class User extends Resource
         return [
             ID::make()->sortable(),
 
-            Gravatar::make()->maxWidth(50),
+            Image::make('Avatar')->disk('s3')->path('avatars'),
 
             Text::make('Name')
                 ->sortable()
                 ->rules('required', 'max:255'),
+
+            Select::make("Gender")->options([
+                'Male' => 'Male',
+                'Female' => 'Female',
+                'None' => 'None',
+            ])->sortable(),
+
+            Text::make('Mobile')
+                ->sortable()
+                ->rules('required', 'max:254'),
+
+            Text::make('Username')
+                ->sortable()
+                ->rules('required', 'max:254', 'regex:/^[\w-]*$/'),
 
             Text::make('Email')
                 ->sortable()
@@ -59,7 +85,35 @@ class User extends Resource
             Password::make('Password')
                 ->onlyOnForms()
                 ->creationRules('required', 'string', 'min:8')
+
                 ->updateRules('nullable', 'string', 'min:8'),
+
+            Text::make('Version')->sortable(),
+
+            HasOne::make('Wallet'),
+
+            BelongsTo::make("Country"),
+
+            BelongsToMany::make('Topics', 'topics', Topic::class)->searchable(),
+
+            BelongsToMany::make('Followers', 'followers', User::class)->searchable(),
+
+            BelongsToMany::make('Followings', 'followings', User::class)->searchable(),
+
+            HasMany::make("Quizzes", "quizzes", QuizParticipant::class),
+
+            Date::make('Joined On', "created_at")
+                ->exceptOnForms()
+                ->resolveUsing(function ($date) {
+                    return $date->format('d/m/Y h:m A');
+                })
+                ->sortable(),
+
+            Boolean::make('Admin'),
+
+            Boolean::make('Demo'),
+
+            Boolean::make('Status'),
         ];
     }
 
@@ -104,6 +158,6 @@ class User extends Resource
      */
     public function actions(Request $request)
     {
-        return [];
+        return [new WalletPoint()];
     }
 }
