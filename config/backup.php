0b006360503ffe0c5b32249327afe05bc8004ff4
Krunal Dodiya <kunal.dodiya1@gmail.com>
2021-01-03 09:27:12 +0530

    added follow admin

--- config/backup.php
+++ config/backup.php
@@ -80,18 +80,7 @@ return [
              * For a complete list of available customization options, see https://github.com/spatie/db-dumper
              */
             'databases' => [
-                'pgsql' => [
-                    'dump' => [
-                        'excludeTables' => [
-                            'action_events',
-                            'websockets_statistics_entries',
-                            'telescope_entries',
-                            'telescope_entries_tags',
-                            'telescope_monitoring',
-                            'migrations'
-                        ]
-                    ],
-                ]
+                'pgsql',
             ],
         ],
 
