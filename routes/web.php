5929bc0d7d8120e75812bf2d89b22220ad8b5e18
krunal dodiya <kunal.dodiya1@gmail.com>
2021-01-21 07:27:46 +0530

    added script to export quiz rankings

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,49 @@
 <?php
 
 use App\QuizRanking;
-use App\User;
 use Illuminate\Support\Facades\Route;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function (Request $request) {
     return view('welcome');
 });
 
 Route::get('/test', function (Request $request) {
-    $balance = QuizRanking::selectRaw('sum(prize) as total')->whereColumn('user_id', 'users.id');
+    if ($request->action === "truncate") {
+        DB::table('quiz_rankings_improved')->truncate();
+    }
 
-    $users = User::addSelect(['balance' => $balance])
-        ->orderBy('balance', 'DESC')
-        ->get();
+    if ($request->action === "insert") {
+        $data = QuizRanking::query()
+            ->where('prize', '>', 0)
+            ->where(function ($query) use ($request) {
+                if ($request->period !== "All Time") {
+                    return $query->where(
+                        'created_at',
+                        '>',
+                        $request->period === "This Month" ? now()->startOfMonth() : now()->startOfDay()
+                    );
+                }
+            })
+            ->get()
+            ->groupBy('user_id')
+            ->map(function ($collection) {
+                return [
+                    'id' => Illuminate\Support\Str::uuid(),
+                    'user_id' => $collection[0]->user_id,
+                    'prize' => $collection->sum('prize'),
+                    'period' => now()->endOfMonth(),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            })->toArray();
 
-    return $users;
+        DB::table('quiz_rankings_improved')->insert($data);
+    }
+
+    return 'done';
 });
 
 Route::get('/refer', function (Request $request) {
